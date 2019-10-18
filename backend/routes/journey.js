74955bbe720a159fa55e21c8a0a151532ab071d5
stev <pedersenstephan@hotmail.com>
2019-10-18 23:48:27 +0200

    journey REST endpoint, create_journey

--- backend/routes/journey.js
+++ backend/routes/journey.js
@@ -37,4 +37,25 @@ router.get("/journeys", (req, res) => {
     })
 })
 
+// Post journey
+router.post("/create_journey", (req, res) => {
+    const journey_checkin = req.body.journey_checkin.toString();
+    const journey_checkout = req.body.journey_checkout.toString();
+    const journey_longtitudestart = res.body.journey_longtitudestart.toString();
+    const journey_latitudestart = res.body.journey_latitudestart.toString();
+    const journey_longtitudeend = res.body.journey_longtitudeend.toString();
+    const journey_latitudeend = res.body.journey_latitudeend.toString();
+
+    const queryString = "INSERT INTO `journey` (journey_checkin, journey_checkout, journey_longtitudestart, journey_latitudestart, journey_longtitudeend, journey_latitudeend) VALUES (?, ?, ?, ?, ?, ?);"
+    getConnection().query(queryString, [journey_checkin, journey_checkout, journey_longtitudestart, journey_latitudestart, journey_longtitudeend, journey_latitudeend], (err, results) => {
+        if (err) {
+            console.log("log --> Failed to query: /user_create " + err)
+            res.sendStatus(500)
+            return
+        }
+        console.log("log --> create new user: /user_create created successfully")
+        res.end()
+    })
+})
+
 module.exports = router
